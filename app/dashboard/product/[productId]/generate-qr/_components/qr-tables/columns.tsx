4beeb7a3bd76704b63d7ccf8db415813ea859d6b
Rohit <rohit.gautam3@cars24.com>
2024-12-08 13:32:57 +0530

    qr npm added

--- app/dashboard/product/[productId]/generate-qr/_components/qr-tables/columns.tsx
+++ app/dashboard/product/[productId]/generate-qr/_components/qr-tables/columns.tsx
@@ -1,45 +1,46 @@
-'use client';
-import { Product } from '@/constants/data';
-import { ColumnDef } from '@tanstack/react-table';
-import Image from 'next/image';
+"use client";
+import { Product } from "@/constants/data";
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+import QRCode from "react-qr-code";
 // import { CellAction } from './cell-action';
 
 export const columns: ColumnDef<Product>[] = [
   {
-    accessorKey: 'photo_url',
-    header: 'IMAGE',
+    accessorKey: "photo_url",
+    header: "QR-IMAGE",
     cell: ({ row }) => {
       return (
-        <div className="relative aspect-square">
-          <Image
-            src={row.getValue('photo_url')}
-            alt={row.getValue('name')}
-            fill
-            className="rounded-lg"
+        <div className="relative w-max">
+          <QRCode
+            size={100}
+            style={{ height: "auto", maxWidth: "80px", width: "80px" }}
+            value={"https://www.youtube.com/watch?v=NkwFxeHARqc"}
+            viewBox={`0 0 256 256`}
           />
         </div>
       );
-    }
+    },
   },
   {
-    accessorKey: 'qr-id',
-    header: 'QR-ID'
+    accessorKey: "qr-id",
+    header: "QR-ID",
   },
   {
-    accessorKey: 'qr-link',
-    header: 'QR LINK'
+    accessorKey: "qr-link",
+    header: "QR LINK",
   },
   {
-    accessorKey: 'qr-download',
-    header: 'DOWNLOAD'
+    accessorKey: "qr-download",
+    header: "DOWNLOAD",
   },
-//   {
-//     accessorKey: 'description',
-//     header: 'DESCRIPTION'
-//   },
+  //   {
+  //     accessorKey: 'description',
+  //     header: 'DESCRIPTION'
+  //   },
 
-//   {
-//     id: 'actions',
-//     cell: ({ row }) => <CellAction data={row.original} />
-//   }
+  //   {
+  //     id: 'actions',
+  //     cell: ({ row }) => <CellAction data={row.original} />
+  //   }
 ];
